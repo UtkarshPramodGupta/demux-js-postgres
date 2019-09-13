0ef9b304f38ca1b5e723d29a924a9b63b7c56512
Utkarsh Gupta <utkarshpramodgupta@gmail.com>
2019-09-13 17:22:04 +0530

    Fixed CommonJS importing
    `massive` is written in CommonJS not typescript. Importing it as before wouldn't import anything and you would be getting `massive` as `undefined`.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+declare function require(name:string):any;
 export { MassiveActionHandler } from './MassiveActionHandler'
 export { Migration } from './Migration'
 export { MigrationRunner } from './MigrationRunner'
 export { MigrationSequence, MassiveActionHandlerOptions } from './interfaces'
-export { default as massive } from 'massive'
+const massive = require('./massive');
+export { massive }
